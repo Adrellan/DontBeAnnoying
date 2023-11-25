ed6e56181b32e5987298243283076279918e1347
Fekete Adrian <2001adrian.fekete@gmail.com>
2023-11-25 04:25:46 +0100

    primefaces + navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+import { PrimeFacesModule } from 'src/primefaces.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './part/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    PrimeFacesModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
